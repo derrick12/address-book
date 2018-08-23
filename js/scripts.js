5f484a68fe71928fea5b4c14b9030ab75cd2e4ac
derrick12 <derrickwaweru@gmail.com>
2018-08-23 15:34:42 +0300

    add bootsrap and jquery links

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,10 @@ function Contact(first, last) {
     this.firstName = first;
     this.lastName = last;
 }
+//returns  the fullname
+Contact.prototype.fullName = function() {
+    return this.firstName + " " + this.lastName;
+}
 
 // user interface logic
 $(document).ready(function() {
@@ -14,9 +18,20 @@ $(document).ready(function() {
 
         var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-        $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+        $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+        $(".contact").last().click(function() {
+            $("#show-contact").show();
+            $("#show-contact h2").text(newContact.firstName);
+            $(".first-name").text(newContact.firstName);
+            $(".last-name").text(newContact.lastName);
+
+            // If we didn't add last() to $(".contact"), each time a new contact was added, every element with the contact class would show the information of the most recently added contact on click.
+        });
 
         $("input#new-first-name").val("");
         $("input#new-last-name").val("");
+
+
     });
 });
