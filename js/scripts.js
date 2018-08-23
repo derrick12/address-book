7c8d1021ee05db609279c6f37be9027bb2061f87
derrick12 <derrickwaweru@gmail.com>
2018-08-23 16:46:59 +0300

    add adsress function

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,13 @@
 function Contact(first, last) {
     this.firstName = first;
     this.lastName = last;
+    this.addresses = [];
+}
+
+function Address(street, city, county) {
+    this.street = street;
+    this.city = city;
+    this.county = county;
 }
 //returns  the fullname
 Contact.prototype.fullName = function() {
